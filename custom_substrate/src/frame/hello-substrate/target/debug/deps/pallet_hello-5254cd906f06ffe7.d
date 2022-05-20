4363fd6649969f151757f6e8dbdacc7e03d13fe3
gitcoin_yoon <socozy7210@gmail.com>
2022-05-20 21:16:21 +0900

    Delete custom_substrate/src/frame/hello-substrate/target directory

--- custom_substrate/src/frame/hello-substrate/target/debug/deps/pallet_hello-5254cd906f06ffe7.d
+++
@@ -1,5 +0,0 @@
-/Users/yoon/dev/Rust/custom_substrate/src/frame/hello-substrate/target/debug/deps/pallet_hello-5254cd906f06ffe7.rmeta: src/lib.rs
-
-/Users/yoon/dev/Rust/custom_substrate/src/frame/hello-substrate/target/debug/deps/pallet_hello-5254cd906f06ffe7.d: src/lib.rs
-
-src/lib.rs:
--- custom_substrate/src/frame/hello-substrate/target/debug/deps/pallet_hello-c24b97428cfb4679.d
+++
@@ -1,5 +0,0 @@
-/Users/yoon/dev/Rust/custom_substrate/src/frame/hello-substrate/target/debug/deps/pallet_hello-c24b97428cfb4679.rmeta: src/lib.rs
-
-/Users/yoon/dev/Rust/custom_substrate/src/frame/hello-substrate/target/debug/deps/pallet_hello-c24b97428cfb4679.d: src/lib.rs
-
-src/lib.rs:
